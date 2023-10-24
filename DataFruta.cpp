c748b313cbad31fb6b0822cc4fdcd0353b31a9ae
SnugbXResidencia <santoseverlan45@gmail.com>
2023-10-24 15:24:44 -0300

    Organizando o codigo

--- DataFruta.cpp
+++ DataFruta.cpp
@@ -7,35 +7,39 @@
 using namespace std;
 
 template <typename T>
-void quicksort(vector<T>& arr, int left, int right) {
+void quicksort(vector<T> &arr, int left, int right)
+{
 
-    if (left < right) {
-        int pivot = left;
-        for (int i = left + 1; i <= right; ++i) {
-            if (arr[i] < arr[left]) {
-                ++pivot;
-                std::swap(arr[pivot], arr[i]);
-            }
-        }
-        std::swap(arr[left], arr[pivot]);
-        quicksort(arr, left, pivot - 1);
-        quicksort(arr, pivot + 1, right);
-    }
+	if (left < right)
+	{
+		int pivot = left;
+		for (int i = left + 1; i <= right; ++i)
+		{
+			if (arr[i] < arr[left])
+			{
+				++pivot;
+				std::swap(arr[pivot], arr[i]);
+			}
+		}
+		std::swap(arr[left], arr[pivot]);
+		quicksort(arr, left, pivot - 1);
+		quicksort(arr, pivot + 1, right);
+	}
 }
 
 // class Data {
 // 	int dia, mes, ano;
 // 	public:
-	
+
 // 	/*
 // 	O m�todo abaixo retornar� -1 se d1 � anterior a d2
 // 	Retornar� 0 se d1 = d2
 // 	Retornar� +1 se d1 � posterior a d2
-// 	*/	
-// 	static int compara(Data d1, Data d2) { 
+// 	*/
+// 	static int compara(Data d1, Data d2) {
 // 		return 0;
 // 	}
-	
+
 // 	Data (int _dia, int _mes, int _ano) {
 // 		dia = _dia;
 // 		mes = _mes;
@@ -51,82 +55,92 @@ void quicksort(vector<T>& arr, int left, int right) {
 // 		return ret;
 // 	}
 // };
-class Lista {
-	public:
-	virtual void entradaDeDados() =0;
-	virtual void mostraMediana() =0;
-	virtual void mostraMenor() =0;
-	virtual void mostraMaior() =0;
-	virtual void listarEmOrdem() =0;
-	virtual void listar_N_elementos(int) =0;
-	virtual int size() =0;
+class Lista
+{
+public:
+	virtual void entradaDeDados() = 0;
+	virtual void mostraMediana() = 0;
+	virtual void mostraMenor() = 0;
+	virtual void mostraMaior() = 0;
+	virtual void listarEmOrdem() = 0;
+	virtual void listar_N_elementos(int) = 0;
+	virtual int size() = 0;
 };
 
-class ListaNomes : public Lista {
+class ListaNomes : public Lista
+{
 	vector<string> lista;
-	
-	public:
-	
+
+public:
 	/*
 	O m�todo abaixo pergunta ao usu�rios quantos
 	elementos v�o existir na lista e depois
 	solicita a digita��o de cada um deles
-	*/	
-	void entradaDeDados() {
+	*/
+	void entradaDeDados()
+	{
 		lista.push_back("Teste3");
 		lista.push_back("Teste1");
 		lista.push_back("Teste2");
 		lista.push_back("Teste");
 	}
-		
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de strings" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar o primeiro nome alfabeticamente" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar o ultimo nome alfabeticamente" << endl;
 	}
 
-	void listarEmOrdem() {
+	void listarEmOrdem()
+	{
 		quicksort<string>(lista, 0, lista.size() - 1);
 		listar_N_elementos(lista.size());
 	}
 
-	void listar_N_elementos(int n) {
+	void listar_N_elementos(int n)
+	{
 		int tam = lista.size();
-		for (int i = 0; i < tam; i++) {
-			if (i < n) {
+		for (int i = 0; i < tam; i++)
+		{
+			if (i < n)
+			{
 				cout << "Nome " << i + 1 << "ª: " << lista[i] << endl;
 			}
 		}
 	}
 
-	int size() {
+	int size()
+	{
 		return lista.size();
 	}
 };
 
 // class ListaDatas  {
 // 	vector<Data> lista;
-	
+
 // 	public:
-		
+
 // 	/*
 // 	O m�todo abaixo pergunta ao usu�rios quantos
 // 	elementos v�o existir na lista e depois
 // 	solicita a digita��o de cada um deles
-// 	*/	
+// 	*/
 // 	void entradaDeDados() {
-		
+
 // 	}
-	
+
 // 	void mostraMediana() {
 // 		cout << "Aqui vai mostrar a mediana da lista de datas" << endl;
 // 	}
-	
+
 // 	void mostraMenor() {
 // 		cout << "Aqui vai mostrar a primeira data cronologicamente" << endl;
 // 	}
@@ -134,145 +148,77 @@ class ListaNomes : public Lista {
 // 		cout << "aqui vai mostrar a ultima data cronologicamente" << endl;
 // 	}
 // };
-class ListaSalarios : public Lista {
+class ListaSalarios : public Lista
+{
 	vector<float> lista;
-	
-	public:
-	
-	int size() override {
-		return lista.size();
-	}
+
+public:
+	int size() override;
 
 	/*
 	O m�todo abaixo pergunta ao usu�rios quantos
 	elementos v�o existir na lista e depois
 	solicita a digita��o de cada um deles
-	*/	
-	void entradaDeDados() override {
-		float valor;
-		int n;
-		cout << "informe a quantidade de salarios que deseja inserir: " << endl;
-		cin >> n;
-		for (int i = 0; i < n; i++) {
-			cout << "informe o valor do salario: " << endl;
-			cin >> valor;
-			lista.push_back(valor);
-		}
-	}
-			
-	void mostraMediana() override {
-		float mediana;
-		if (lista.size() % 2 == 0) {
-			mediana = (lista[(lista.size() - 1) / 2] + lista[(lista.size() - 1) / 2 + 1]) / 2;
-		}
-		else{
-			mediana = lista[(lista.size() - 1) / 2];
-		}
-		cout << "\tMediana: " << mediana << endl;
-	}
-	
-	void mostraMenor() override {
-		float menor = lista[0];
-		int tam = lista.size();
-		for (int i = 1; i < tam; i++) {
-			if (lista[i] < menor) {
-				menor = lista[i];
-			}			
-		}
-		cout << "\tMenor salario: " << menor << endl;
-	}
-	void mostraMaior() override {
-		float maior = lista[0];
-		int tam = lista.size();
-		for (int i = 1; i < tam; i++) {
-			if (lista[i] > maior) {
-				maior = lista[i];
-			}			
-		}
-		cout << "\tMaior salario: " << maior << endl;
-	}
+	*/
+	void entradaDeDados() override;
 
-	void listarEmOrdem() override {
-		int tam = lista.size();
-		quicksort<float>(lista, 0, lista.size() - 1);
-		listar_N_elementos(tam);
-	}
+	void mostraMediana() override;
 
-	void listar_N_elementos(int n) override {
-		int tam = lista.size();
-		for (int i = 0; i < tam; i++) {
-			if (i < n) {
-				cout << "\tSalario " << i + 1 << "ª: " << lista[i] << endl;
-			}
-		}
-	}
+	void mostraMenor() override;
+
+	void mostraMaior() override;
+
+	void listarEmOrdem() override;
+
+	void listar_N_elementos(int n) override;
 };
 
-
-class ListaIdades  {
+class ListaIdades
+{
 	vector<int> lista;
-	
-	public:
-		
-		/*
-	O m�todo abaixo pergunta ao usu�rios quantos
-	elementos v�o existir na lista e depois
-	solicita a digita��o de cada um deles
-	*/	
-	void entradaDeDados() {
-		
+
+public:
+	/*
+O m�todo abaixo pergunta ao usu�rios quantos
+elementos v�o existir na lista e depois
+solicita a digita��o de cada um deles
+*/
+	void entradaDeDados()
+	{
 	}
-	
-	void mostraMediana() {
+
+	void mostraMediana()
+	{
 		cout << "Aqui vai mostrar a mediana da lista de idades" << endl;
 	}
-	
-	void mostraMenor() {
+
+	void mostraMenor()
+	{
 		cout << "Aqui vai mostrar a menor das idades" << endl;
 	}
-	void mostraMaior() {
+	void mostraMaior()
+	{
 		cout << "aqui vai mostrar a maior das idades" << endl;
 	}
 };
- 
- void limpaTela()
+
+//// FUNCAO de Utilidade ////
+void limpaTela();
+void pause();
+
+int main()
 {
-    cin.clear();
-
-    // Verifica se a variável de ambiente WINDIR está definida (ambiente Windows)
-    const char *windir = getenv("WINDIR");
-
-    if (windir != nullptr && strlen(windir) > 0)
-    {
-        // Se estiver definida, estamos no Windows, então use "cls"
-        system("cls");
-    }
-    else
-    {
-        // Caso contrário, estamos em um ambiente Unix/Linux, use "clear"
-        system("clear");
-    }
-}
-
-void pause()
-{
-    cin.ignore();
-    cout << "\nPressione a tecla Enter para continuar...\n";
-    cin.ignore(numeric_limits<streamsize>::max(), '\n');
-}
-
-int main () {
 	limpaTela();
-	vector<Lista*>listaDeListas;
-	
+	vector<Lista *> listaDeListas;
+
 	ListaNomes listaNomes;
 	listaNomes.entradaDeDados();
 	listaDeListas.push_back(&listaNomes);
-	
+
 	// ListaDatas listaDatas;
 	// listaDatas.entradaDeDados();
 	// listaDeListas.push_back(&listaDatas);
-	
+
 	ListaSalarios listaSalarios;
 	listaSalarios.entradaDeDados();
 	listaDeListas.push_back(&listaSalarios);
@@ -282,8 +228,10 @@ int main () {
 	// listaDeListas.push_back(&listaIdades);
 	limpaTela();
 	cout << "################## Lista de salarios ################## " << endl;
-	for(Lista* l : listaDeListas) {
-		if(typeid(*l) == typeid(ListaSalarios)) {
+	for (Lista *l : listaDeListas)
+	{
+		if (typeid(*l) == typeid(ListaSalarios))
+		{
 			cout << "Tamanho: " << l->size() << endl;
 			cout << "Itens: " << endl;
 			l->listar_N_elementos(l->size());
@@ -301,6 +249,117 @@ int main () {
 		}
 	}
 	cout << "+========================================================+" << endl;
-}
-    
 
+}
+
+void limpaTela(){
+	cin.clear();
+
+	// Verifica se a variável de ambiente WINDIR está definida (ambiente Windows)
+	const char *windir = getenv("WINDIR");
+
+	if (windir != nullptr && strlen(windir) > 0)
+	{
+		// Se estiver definida, estamos no Windows, então use "cls"
+		system("cls");
+	}
+	else
+	{
+		// Caso contrário, estamos em um ambiente Unix/Linux, use "clear"
+		system("clear");
+	}
+}
+
+void pause()
+{
+	cin.ignore();
+	cout << "\nPressione a tecla Enter para continuar...\n";
+	cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+///////////////////////////////////////////////////////////////////
+/////// IMPLEMENTACAO DOS METODOS DA CLASSE ListaSalarios /////////
+///////////////////////////////////////////////////////////////////
+void ListaSalarios::entradaDeDados()
+{
+	float valor;
+	int n;
+	cout << "informe a quantidade de salarios que deseja inserir: " << endl;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cout << "informe o valor do salario: " << endl;
+		cin >> valor;
+		lista.push_back(valor);
+	}
+}
+
+void ListaSalarios::mostraMediana()
+{
+	float mediana;
+	if (lista.size() % 2 == 0)
+	{
+		mediana = (lista[(lista.size() - 1) / 2] + lista[(lista.size() - 1) / 2 + 1]) / 2;
+	}
+	else
+	{
+		mediana = lista[(lista.size() - 1) / 2];
+	}
+	cout << "\tMediana: " << mediana << endl;
+}
+
+void ListaSalarios::mostraMenor()
+{
+	float menor = lista[0];
+	int tam = lista.size();
+	for (int i = 1; i < tam; i++)
+	{
+		if (lista[i] < menor)
+		{
+			menor = lista[i];
+		}
+	}
+	cout << "\tMenor salario: " << menor << endl;
+}
+
+void ListaSalarios::mostraMaior()
+{
+	float maior = lista[0];
+	int tam = lista.size();
+	for (int i = 1; i < tam; i++)
+	{
+		if (lista[i] > maior)
+		{
+			maior = lista[i];
+		}
+	}
+	cout << "\tMaior salario: " << maior << endl;
+}
+
+void ListaSalarios::listarEmOrdem()
+{
+	int tam = lista.size();
+	quicksort<float>(lista, 0, lista.size() - 1);
+	listar_N_elementos(tam);
+}
+
+void ListaSalarios::listar_N_elementos(int n)
+{
+	int tam = lista.size();
+	for (int i = 0; i < tam; i++)
+	{
+		if (i < n)
+		{
+			cout << "\tSalario " << i + 1 << "ª: " << lista[i] << endl;
+		}
+	}
+}
+
+int ListaSalarios::size()
+{
+	return lista.size();
+}
+
+///////////////////////////////////////////////////////////////////
+///// FINAL IMPLEMENTACAO DOS METODOS DA CLASSE ListaSalarios /////
+///////////////////////////////////////////////////////////////////
